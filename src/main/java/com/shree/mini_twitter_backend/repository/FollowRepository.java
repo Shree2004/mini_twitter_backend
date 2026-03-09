c2fcdfce1dfaf3da4499faa7415b7d23b96a30b3
Shree2004 <shreeshinde3007@gmail.com>
2026-03-09 15:22:30 +0530

    Added repositories and methods

---
+++ src/main/java/com/shree/mini_twitter_backend/repository/FollowRepository.java
@@ -0,0 +1,9 @@
+package com.shree.mini_twitter_backend.repository;
+
+import com.shree.mini_twitter_backend.entity.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+}
---
+++ src/main/java/com/shree/mini_twitter_backend/repository/PostRepository.java
@@ -0,0 +1,20 @@
+package com.shree.mini_twitter_backend.repository;
+
+import com.shree.mini_twitter_backend.entity.Post;
+import com.shree.mini_twitter_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByUser(User user);
+
+    List<Post> findByUserOrderByCreatedAtDesc(User user);
+
+    List<Post> findAllByOrderByCreatedAtDesc();
+
+    List<Post> findByContentContainingIgnoreCase(String keyword);
+}
