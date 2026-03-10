03dd12bf0973cb885e598e06806a21437fcb113d
Shree2004 <shreeshinde3007@gmail.com>
2026-03-10 20:07:06 +0530

    Created API for like unlike and get likes count

---
+++ src/main/java/com/shree/mini_twitter_backend/controller/LikeController.java
@@ -0,0 +1,28 @@
+package com.shree.mini_twitter_backend.controller;
+
+import com.shree.mini_twitter_backend.entity.Like;
+import com.shree.mini_twitter_backend.service.LikeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class LikeController {
+
+    @Autowired
+    private LikeService likeService;
+
+    @PostMapping("/users/{userId}/likes/{postId}")
+    public Like likePost(@PathVariable Long userId, @PathVariable Long postId) {
+        return likeService.likePost(userId, postId);
+    }
+
+    @DeleteMapping("/users/{userId}/unlikes/{postId}")
+    public void unLikePost(@PathVariable Long userId, @PathVariable Long postId) {
+        likeService.unLikePost(userId, postId);
+    }
+
+    @GetMapping("/posts/{postId}/likes")
+    public Long getLikesCount(@PathVariable Long postId) {
+        return likeService.getLikesCount(postId);
+    }
+}
