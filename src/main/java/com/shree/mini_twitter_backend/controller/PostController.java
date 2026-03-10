1eb79eae6d37c56177f6d97e75e987c285d0c995
Shree2004 <shreeshinde3007@gmail.com>
2026-03-10 15:28:06 +0530

    Added API of create post , find post by id , get a users posts and delete post

---
+++ src/main/java/com/shree/mini_twitter_backend/controller/PostController.java
@@ -0,0 +1,37 @@
+package com.shree.mini_twitter_backend.controller;
+
+import com.shree.mini_twitter_backend.entity.Post;
+import com.shree.mini_twitter_backend.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping("/users/{userId}/posts")
+    public Post createPost(@PathVariable Long userId, @RequestParam String content, @RequestParam MultipartFile image) throws IOException {
+        return postService.createPost(userId,content,image);
+    }
+
+    @GetMapping("/posts/{postId}")
+    public Post getPostById(@PathVariable Long postId){
+        return postService.getPostById(postId);
+    }
+
+    @GetMapping("/users/{userId}/posts")
+    public List<Post> getPostsByUserId(@PathVariable Long userId){
+        return postService.getPostsByUserId(userId);
+    }
+
+    @DeleteMapping("/users/{userId}/posts/{postId}")
+    public void deletePost(@PathVariable Long userId, @PathVariable Long postId){
+        postService.deletePost(userId, postId);
+    }
+}
