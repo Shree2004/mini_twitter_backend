a45ad52141e680156374235c1537b3862c67240c
Shree2004 <shreeshinde3007@gmail.com>
2026-03-10 23:55:18 +0530

    Added API of Comment

---
+++ src/main/java/com/shree/mini_twitter_backend/controller/CommentController.java
@@ -0,0 +1,30 @@
+package com.shree.mini_twitter_backend.controller;
+
+import com.shree.mini_twitter_backend.entity.Comment;
+import com.shree.mini_twitter_backend.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping("/users/{userId}/posts/{postId}/comments")
+    public Comment createComment(@PathVariable Long userId, @PathVariable Long postId, @RequestParam String content) {
+        return commentService.createComment(userId, postId, content);
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public List<Comment> getCommentsByPost(@PathVariable Long postId) {
+        return commentService.getCommentsByPost(postId);
+    }
+
+    @DeleteMapping("/users/{userId}/posts/{postId}/comments/{commentId}")
+    public void deleteComment(@PathVariable Long userId, @PathVariable Long postId, @PathVariable Long commentId) {
+        commentService.deleteComment(userId, postId, commentId);
+    }
+}
