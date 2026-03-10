a13e234bd34c79e118b52b7d242ff00a925ddbb4
Shree2004 <shreeshinde3007@gmail.com>
2026-03-10 17:42:31 +0530

    Added Like , unlike and see all likes in a post

--- src/main/java/com/shree/mini_twitter_backend/repository/LikeRepository.java
+++ src/main/java/com/shree/mini_twitter_backend/repository/LikeRepository.java
@@ -1,9 +1,15 @@
 package com.shree.mini_twitter_backend.repository;
 
 import com.shree.mini_twitter_backend.entity.Like;
+import com.shree.mini_twitter_backend.entity.Post;
+import com.shree.mini_twitter_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    boolean existsByUserAndPost(User user, Post post);
+    Long countByPost(Long postId);
+    Optional<Like> findByUserAndPost(User user, Post post);
 }
---
+++ src/main/java/com/shree/mini_twitter_backend/service/LikeService.java
@@ -0,0 +1,67 @@
+package com.shree.mini_twitter_backend.service;
+
+import com.shree.mini_twitter_backend.entity.Like;
+import com.shree.mini_twitter_backend.entity.Post;
+import com.shree.mini_twitter_backend.entity.User;
+import com.shree.mini_twitter_backend.repository.LikeRepository;
+import com.shree.mini_twitter_backend.repository.PostRepository;
+import com.shree.mini_twitter_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LikeService {
+
+    @Autowired
+    private LikeRepository likeRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PostRepository postRepository;
+
+
+    public Like likePost(Long userId, Long postId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new RuntimeException("User Not Found"));
+
+        Post post = postRepository.findById(postId)
+                .orElseThrow(()-> new RuntimeException("Post not Found"));
+
+        if (likeRepository.existsByUserAndPost(user,post)){
+            throw new RuntimeException("You Already liked this post");
+        }
+
+        Like like = new Like();
+        like.setUser(user);
+        like.setPost(post);
+
+        return likeRepository.save(like);
+    }
+
+    public void unLikePost(Long userId, Long postId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new RuntimeException("User Not Found"));
+
+        Post post = postRepository.findById(postId)
+                .orElseThrow(()-> new RuntimeException("Post not Found"));
+
+        Like like = likeRepository.findByUserAndPost(user, post)
+                .orElseThrow(() -> new RuntimeException("Like not found"));
+
+        likeRepository.delete(like);
+
+    }
+
+    public Long getLikesCount(Long postId){
+
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new RuntimeException("Post not Found"));
+
+        return likeRepository.countByPost(post.getPostId());
+    }
+
+
+
+}
