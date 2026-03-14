96f8b2a656a2d7ef0b719e3dbaa74d735a17a9cd
Shree2004 <shreeshinde3007@gmail.com>
2026-03-15 00:56:04 +0530

    Add corsconfig

---
+++ src/main/java/com/shree/mini_twitter_backend/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.shree.mini_twitter_backend.security;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.cors.*;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
